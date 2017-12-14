842c2424e66944eabb1e555c388033942d3d023d
Mageswaran <mageswaran1989@gmail.com>
2017-12-14 11:13:33 +0530

    adding tagger class

--- src/commands/patent_tagger.py
+++ src/commands/patent_tagger.py
@@ -12,57 +12,68 @@ MODEL_DIR = "/opt/0.imaginea/git/i-tagger/experiments/bilstm_crf_v0/charembd_Tru
 NUM_EPOCHS = 5
 BATCH_SIZE = 2
 
-preprocessor = PatentDataPreprocessor(
-    experiment_root_directory=EXPERIMENT_ROOT_DIR,
-    over_write=None,
-    use_iob=None,
-    out_dir=None,
-    train_csvs_path=None,
-    val_csv_path=None,
-    test_csv_path=None,
-    db_reference_file=None,
-    text_col=None,
-    entity_col=None,
-    do_run_time_config=False)
 
-preprocessor.start()
+class PattentTagger():
+    def __init__(self):
+        self.preprocessor = None
+        self.estimator = None
+        self.data_iterators = None
 
-estimator_config, estimator = TFEstimatorFactory.get("bilstm_crf_v0")
-if MODEL_DIR:
-    config = estimator_config.load(MODEL_DIR)
-    if config == None: #Fail safe
-        estimator_config = estimator_config.with_user_hyperparamaters(EXPERIMENT_ROOT_DIR, preprocessor.OUT_DIR)
-    else:
-        estimator_config = config
-else:
-    estimator_config = estimator_config.with_user_hyperparamaters(EXPERIMENT_ROOT_DIR, preprocessor.OUT_DIR)
-estimator = estimator(estimator_config)
+    def init(self):
+        self.preprocessor = PatentDataPreprocessor(
+            experiment_root_directory=EXPERIMENT_ROOT_DIR,
+            over_write=None,
+            use_iob=None,
+            out_dir=None,
+            train_csvs_path=None,
+            val_csv_path=None,
+            test_csv_path=None,
+            db_reference_file=None,
+            text_col=None,
+            entity_col=None,
+            do_run_time_config=False)
 
-data_iterators = PatentIDataIterator(preprocessor.OUT_DIR, batch_size=BATCH_SIZE)
-data_iterators.prepare()
+        estimator_config, estimator = TFEstimatorFactory.get("bilstm_crf_v0")
+        if MODEL_DIR:
+            config = estimator_config.load(MODEL_DIR)
+            if config == None: #Fail safe
+                estimator_config = estimator_config.with_user_hyperparamaters(EXPERIMENT_ROOT_DIR, self.preprocessor.OUT_DIR)
+            else:
+                estimator_config = config
+        else:
+            estimator_config = estimator_config.with_user_hyperparamaters(EXPERIMENT_ROOT_DIR, self.preprocessor.OUT_DIR)
 
-num_samples = data_iterators.NUM_TRAINING_SAMPLES
+        self.estimator = estimator(estimator_config)
 
-print_info(num_samples)
-max_steps = (num_samples // BATCH_SIZE) * NUM_EPOCHS
-print_info("Total number steps: {} ".format(max_steps))
+        self.data_iterators = PatentIDataIterator(self.preprocessor.OUT_DIR, batch_size=BATCH_SIZE)
 
-for current_epoch in range(NUM_EPOCHS):
-    max_steps = (num_samples // BATCH_SIZE) * (current_epoch + 1)
+    def preprocess(self):
+        self.preprocessor.start()
 
-    train_hooks = []
-    train_hooks.append(data_iterators.train_data_init_hook)
-    # if len(estimator.hooks) > 0:
-    #     train_hooks.extend(tagger.hooks)
+    def train(self):
+        self.data_iterators.prepare()
+        num_samples = self.data_iterators.NUM_TRAINING_SAMPLES
 
-    estimator.train(input_fn=data_iterators.train_data_input_fn,
-             hooks=train_hooks,
-             max_steps=max_steps)
+        print_info(num_samples)
+        max_steps = (num_samples // BATCH_SIZE) * NUM_EPOCHS
+        print_info("Total number steps: {} ".format(max_steps))
 
-    eval_results = estimator.evaluate(input_fn=data_iterators.val_data_input_fn,
-                                   hooks=[data_iterators.val_data_init_hook])  # tf_debug.LocalCLIDebugHook()
+        for current_epoch in range(NUM_EPOCHS):
+            max_steps = (num_samples // BATCH_SIZE) * (current_epoch + 1)
 
-    print(eval_results)
+            train_hooks = []
+            train_hooks.append(self.data_iterators.train_data_init_hook)
+            # if len(estimator.hooks) > 0:
+            #     train_hooks.extend(tagger.hooks)
 
+            self.estimator.train(input_fn=self.data_iterators.train_data_input_fn,
+                     hooks=train_hooks,
+                     max_steps=max_steps)
 
-data_iterators.predict_on_csv_files(estimator=estimator, csv_files_path="data/test/")
+            eval_results = self.estimator.evaluate(input_fn=self.data_iterators.val_data_input_fn,
+                                           hooks=[self.data_iterators.val_data_init_hook])  # tf_debug.LocalCLIDebugHook()
+
+            print(eval_results)
+
+    def predict_on_test_files(self):
+        self.data_iterators.predict_on_csv_files(estimator=self.estimator, csv_files_path="data/test/")
